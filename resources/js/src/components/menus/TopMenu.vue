d5a7b46af15ddc278ab8caa3d0e83f1bf7aa7692
murilomac7 <murilomac7@gmail.com>
2021-03-25 14:44:24 -0300

    pagina quem somos, falta logo preto com fundo transp.

--- resources/js/src/components/menus/TopMenu.vue
+++ resources/js/src/components/menus/TopMenu.vue
@@ -7,9 +7,9 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="mx-auto">
-                    <b-nav-item :to="{name: 'unauthenticated.home'}" >Início</b-nav-item>
-                    <b-nav-item>Quem somos</b-nav-item>
-                    <b-nav-item>Serviços</b-nav-item>
+                    <b-nav-item :to="{name: 'unauthenticated.home'}">Início</b-nav-item>
+                    <b-nav-item :to="{name: 'unauthenticated.quem_somos'}">Quem somos</b-nav-item>
+                    <b-nav-item :to="{name: 'unauthenticated.servicos'}">Serviços</b-nav-item>
                     <b-nav-item :to="{name: 'unauthenticated.agendamento'}">Agendamentos</b-nav-item>
                     <b-nav-item :to="{name: 'unauthenticated.contato'}">Contato</b-nav-item>
                 </b-navbar-nav>
